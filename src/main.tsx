1353491bd764cbe508056c207d218bc4c533a1ca
nrwtkd <nurwitakumala@gmail.com>
2026-07-31 12:56:35 +0700

    Load Atlas onboarding experience styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./login-fit.css";
+import "./experience.css";
 
 if ("serviceWorker" in navigator && import.meta.env.PROD) {
   window.addEventListener("load", () => {
