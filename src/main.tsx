e6034155d53864bb511b705e0ae22904d3921a19
nrwtkd <nurwitakumala@gmail.com>
2026-07-31 17:42:48 +0700

    Load final Atlas UI polish overrides

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import "./universal-v3.css";
 import "./learning-rich-v5.css";
 import "./learning-depth-v6.css";
 import "./onboarding-v7.css";
+import "./ui-polish-v7.css";
 
 if ("serviceWorker" in navigator && import.meta.env.PROD) {
   window.addEventListener("load", () => {
