0e75ac49ac9b6ffe373385d34dab305b15a971eb
nrwtkd <nurwitakumala@gmail.com>
2026-08-01 05:50:31 +0700

    Redesign Tala as expressive sprout mascot

--- src/components/AtlasCompanion.tsx
+++ src/components/AtlasCompanion.tsx
@@ -20,49 +20,39 @@ export default function AtlasCompanion({
   return (
     <span className={`atlasCompanion atlasCompanion--${size} atlasCompanion--${mood}`} role="img" aria-label={label}>
       <svg viewBox="0 0 160 170" aria-hidden="true">
-        <ellipse className="companionShadow" cx="80" cy="154" rx="42" ry="8" />
+        <ellipse className="companionShadow" cx="80" cy="153" rx="39" ry="8" />
 
-        {cheering && (
-          <g className="companionSparkles">
-            <path d="M23 31v14M16 38h14" />
-            <path d="M134 24v16M126 32h16" />
-            <path d="M140 61v10M135 66h10" />
-          </g>
-        )}
+        {cheering && <g className="companionSparkles"><path d="M25 34v14M18 41h14"/><path d="M133 28v15M126 35h14"/></g>}
+
+        <g className="sproutLeaves">
+          <path d="M79 54C61 39 57 21 61 10c17 3 28 17 25 35" />
+          <path d="M82 55c8-21 24-31 39-29 0 17-12 32-34 34" />
+          <path className="sproutStem" d="M81 57c0-17 0-29 2-40" />
+        </g>
 
         <g className="companionArms">
-          <path d={cheering ? "M44 88C27 78 24 61 29 48" : thinking ? "M44 91C31 88 31 75 41 68" : "M44 91C30 98 28 111 34 122"} />
-          <path d={cheering ? "M116 88C133 78 136 61 131 48" : guiding ? "M116 91C132 87 137 74 133 61" : "M116 91C130 98 132 111 126 122"} />
-          <circle cx={cheering ? 29 : thinking ? 41 : 34} cy={cheering ? 47 : thinking ? 68 : 123} r="5" />
-          <circle cx={cheering ? 131 : guiding ? 133 : 126} cy={cheering ? 47 : guiding ? 61 : 123} r="5" />
+          <path d={cheering ? "M51 94C34 83 29 67 33 54" : thinking ? "M52 98C39 95 36 82 45 75" : "M52 98C38 105 35 118 40 129"} />
+          <path d={cheering ? "M109 94c17-11 22-27 18-40" : guiding ? "M109 98c16-5 21-18 17-31" : "M109 98c14 7 17 20 12 31"} />
+          <circle cx={cheering ? 33 : thinking ? 45 : 40} cy={cheering ? 54 : thinking ? 75 : 130} r="5" />
+          <circle cx={cheering ? 127 : guiding ? 126 : 121} cy={cheering ? 54 : guiding ? 67 : 130} r="5" />
         </g>
 
-        {guiding && (
-          <g className="companionFlag">
-            <path d="M135 61V24" />
-            <path d="M135 25h18l-5 9 5 9h-18z" />
-          </g>
-        )}
+        {guiding && <g className="companionFlag"><path d="M126 67V30"/><path d="M126 31h19l-5 9 5 9h-19z"/></g>}
 
-        <path className="companionHandle" d="M61 42c0-15 38-15 38 0" />
-        <rect className="companionBody" x="40" y="39" width="80" height="105" rx="29" />
-        <path className="companionTopPanel" d="M53 51c14-10 40-10 54 0v28H53z" />
-        <rect className="companionFace" x="51" y="58" width="58" height="52" rx="22" />
+        <path className="sproutBody" d="M48 75c7-19 23-28 32-28s25 9 32 28c8 22 2 58-12 70-9 8-31 8-40 0-14-12-20-48-12-70z" />
+        <ellipse className="sproutBelly" cx="80" cy="113" rx="27" ry="25" />
 
         <g className="companionEyes">
-          <circle cx="68" cy="82" r="3.6" />
-          <circle cx="92" cy="82" r="3.6" />
-          {thinking && <path d="M62 73q6-4 12 0M86 73q6 1 12-2" />}
+          <circle cx="68" cy="94" r="3.7" />
+          <circle cx="92" cy="94" r="3.7" />
+          {thinking && <path d="M62 85q6-4 12 0M86 85q6 1 12-2" />}
         </g>
-        <path className="companionSmile" d={cheering ? "M66 92q14 16 28 0" : thinking ? "M69 96q11-5 22 0" : "M68 92q12 10 24 0"} />
+        <path className="companionSmile" d={cheering ? "M65 105q15 17 30 0" : thinking ? "M69 108q11-5 22 0" : "M68 104q12 10 24 0"} />
+        <circle className="sproutBlush" cx="58" cy="105" r="5" />
+        <circle className="sproutBlush" cx="102" cy="105" r="5" />
 
-        <rect className="companionPocket" x="57" y="114" width="46" height="24" rx="10" />
-        <path className="companionStar" d="m80 118 3.6 7.2 8 1.2-5.8 5.6 1.4 7.9-7.2-3.8-7.2 3.8 1.4-7.9-5.8-5.6 8-1.2z" />
-        <path className="companionScarf" d="M45 104c21 12 49 12 70 0l-7 16c-18 9-38 9-56 0z" />
-        <path className="companionScarfTail" d="M105 113c13 5 19 13 20 24-8-5-17-6-25-3z" />
-
-        <path className="companionFoot" d="M59 143v8M101 143v8" />
-        {thinking && <path className="companionQuestion" d="M126 43c0-9 15-10 15 0 0 7-8 6-8 13M133 65h.1" />}
+        <path className="companionFoot" d="M63 145v7M97 145v7" />
+        {thinking && <path className="companionQuestion" d="M126 47c0-9 15-10 15 0 0 7-8 6-8 13M133 69h.1" />}
       </svg>
     </span>
   );
