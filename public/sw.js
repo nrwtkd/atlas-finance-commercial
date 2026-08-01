5bf19c9909334dd5213e5f1c03615ea447e9eae4
nrwtkd <nurwitakumala@gmail.com>
2026-08-01 18:25:33 +0700

    Refresh PWA cache for family nutrition journey

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "atlas-finance-shell-v9";
+const CACHE_NAME = "atlas-finance-shell-v10";
 const APP_SHELL = ["/","/index.html","/manifest.webmanifest","/icons/atlas-icon.svg","/icons/atlas-maskable.svg"];
 self.addEventListener("install",(event)=>{event.waitUntil(caches.open(CACHE_NAME).then((cache)=>cache.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener("activate",(event)=>{event.waitUntil(caches.keys().then((keys)=>Promise.all(keys.filter((key)=>key!==CACHE_NAME).map((key)=>caches.delete(key)))));self.clients.claim();});
